1d61b6a4cfea62ad175ecaa65312ffef2c4ef50a
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-11-20 16:47:54 +0500

    feat: add integration with scalar

--- src/SmartBin.Api/Program.cs
+++ src/SmartBin.Api/Program.cs
@@ -1,7 +1,28 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 
-app.MapGet("/hello", () => "Hello");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference("/docs", options =>
+    {
+        options
+            .WithTitle("SmartBin API")
+            .WithTheme(ScalarTheme.Kepler)
+            .ShowOperationId()
+            .WithDefaultHttpClient(ScalarTarget.JavaScript, ScalarClient.Axios)
+            .AddPreferredSecuritySchemes("BearerAuth")
+            .AddHttpAuthentication("BearerAuth", auth =>
+            {
+                auth.Token = "Your jwt token";
+            });
+    });
+}
+
+app.MapGet("/hello", () => "Hello").Stable();
 app.Run();
